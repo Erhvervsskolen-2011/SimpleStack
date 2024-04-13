12dc0fd7b1227ae11a800f7bffe9a6a15b66483f
Søren Magnusson <soren.magnusson@dinstu.dk>
2024-04-13 09:10:34 +0200

    read ok

--- webserver/index.js
+++ webserver/index.js
@@ -5,15 +5,37 @@ let app = express();
 
 app.set('view engine', 'ejs');
 
+// fra første demo... bruges ikke rigtigt af denne demo
 app.get('/', (req, res) => {
   res.render('index', {foo: 'FOO'});
 });
 
-let observations = [{id: Date(), condition: "fair"}, 
-                    {id: Date(), condition: "good"}
+// Denne Array simulerer en engentlig datakilde
+// Data indlæses når web-sitet startes med f.eks. `node index.js`
+// Alle ændringer forsvinder når scriptet stopper
+
+let observations = [{id: Date.now().toPrecision(), condition: "fair"}, 
+                    {id: Date.now().valueOf()+1,   condition: "good"}
                 ]
+
+
 app.get('/weather', (req, res) => {
     res.render('observations', {obs: observations})
+})
+
+app.get('/weather/edit', (req, res) => {
+
+    const id = req.query.id
+    const obs = observations.find( o => o.id == id ) 
+
+    // console.log('obs: ' + JSON.parse(JSON.stringify(obs)))
+
+    // console.log('req.path: ' + JSON.parse(JSON.stringify(req.path)))
+    // const path = req.path
+    // const action = 'path.slice(path.lastIndexOf('/')+1)'
+    // console.log(action)
+    const action = 'edit'
+    res.render('observation_form', {obs: obs, action: action})
 })
 
 app.listen(8080, () => console.log('Example app listening on port 8080!'));
