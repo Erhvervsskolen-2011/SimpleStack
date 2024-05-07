dcd8bc0a37c2d6d6fe3e5aa7a362a9ecd85ec683
Søren Magnusson <soren.magnusson@dinstu.dk>
2024-05-07 17:24:50 +0200

    Full CRUD

--- webserver/index.js
+++ webserver/index.js
@@ -29,16 +29,6 @@ app.get('/', (req, res) => {
   res.render('index', {foo: 'FOO'});
 });
 
-// Denne Array simulerer en engentlig datakilde
-// Data indlæses når web-sitet startes med f.eks. `node index.js`
-// Alle ændringer forsvinder når scriptet stopper
-// Som `id` bruger vi den numeriske værdi for `Date.now()`
-
-let observations = [{id: Date.now().valueOf(), condition: "fair"}, 
-                    {id: Date.now().valueOf()+1,   condition: "good"}
-                ]
-
-
 app.get('/weather', (req, res) => {
     // let result;
     client.query('SELECT * FROM weather_observations ORDER BY id DESC', (err, result) => {
@@ -53,10 +43,7 @@ app.get('/weather', (req, res) => {
 })
 
 app.get('/weather/update', (req, res) => {
-
     const id = req.query.id
-    // Hent et element i listen efter id. Svarer til SELECT i SQL
-    // const obs = observations.find( o => o.id == id ) 
     const action = 'update'
     
     const sql = "SELECT * FROM weather_observations WHERE id = $1"
@@ -80,10 +67,17 @@ app.get('/weather/create', (req, res) => {
 
 app.get('/weather/delete', (req, res) => {
     const id = req.query.id
-    // Hent et element i listen efter id. Svarer til SELECT i SQL
-    const obs = observations.find( o => o.id == id ) 
-    const action = 'delete'
-    res.render('observation_form', {obs: obs, action: action})
+    const action = 'delete'    
+    const sql = "SELECT * FROM weather_observations WHERE id = $1"
+    const values = [id] 
+    client.query(sql, values, (err, result) => {
+        if (err) {
+            console.error('Error executing query', err);
+        } else {
+            console.log('Query result:', result.rows);
+            res.render('observation_form', {obs: result.rows[0], action: action})
+        }
+    });
 })
 
 // disse to linjer putter data i req.body
@@ -92,10 +86,7 @@ app.use(express.urlencoded({ extended: true })) // for parsing application/x-www
 
 app.post('/weather/update', (req, res) => {
     console.log('req.body: ', req.body)
-    // res.json(req.body)
     
-    // console.log("observations: ", observations)
-
     const id = req.body.id
     const condition = req.body.condition
     
@@ -111,21 +102,16 @@ app.post('/weather/update', (req, res) => {
         }
     });
 	
-
-    // console.log("observations': ", observations)
-
     res.redirect('/weather')
 })
 
 app.post('/weather/create', (req, res) => {
     console.log('req.body: ', req.body)
-    console.log("observations: ", observations)
+    // console.log("observations: ", observations)
 
     const id = Date.now().valueOf()
     const condition = req.body.condition
-    // const obs = {id: id, condition: condition}
-    // observations.push(obs)
-
+    
     const sql =
         'INSERT INTO weather_observations(id, condition) VALUES ($1, $2) RETURNING *    ';
     const values = [id, condition];
@@ -138,33 +124,28 @@ app.post('/weather/create', (req, res) => {
         }
     });
 	
-    // console.log("observations': ", observations)
-
     res.redirect('/weather')
 })
 
 app.post('/weather/delete', (req, res) => {
     console.log('req.body: ', req.body)
-    console.log("observations: ", observations)
 
     const id = req.body.id
-    const i = observations.findIndex( o => o.id == id )
-    // delete with splice (wierd...), see https://stackoverflow.com/questions/500606/deleting-array-elements-in-javascript-delete-vs-splice
-    observations.splice(i, 1);
 
-    console.log("observations': ", observations)
-
+    const sql = "DELETE FROM weather_observations WHERE id = $1"
+    const values = [id] 
+    client.query(sql, values, (err, result) => {
+        if (err) {
+            console.error('Error executing query', err);
+        } else {
+            console.log('sql: ', sql);
+            console.log('body: ', req.body);
+            console.log('id: ', id);
+            console.log('Query result:', result.rows);
+        }
+    });
+    
     res.redirect('/weather')
 })
 
-
 app.listen(8080, () => console.log('Example app listening on port 8080!'));
-
-// client
-// 	.end()
-// 	.then(() => {
-// 		console.log('Connection to PostgreSQL closed');
-// 	})
-// 	.catch((err) => {
-// 		console.error('Error closing connection', err);
-// 	});
